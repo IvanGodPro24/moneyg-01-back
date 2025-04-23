4c5d3166b837413d1b4a0423627e128a1996fadb
Dmitry Naumenko <dimanaumenko31@gmail.com>
2025-04-23 18:52:19 +0300

    transactionsGet

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import authRegister from './auth/authRegister.js';
 import authLogin from './auth/authLogin.js';
 import authLogout from './auth/authLogout.js';
+import getTransactions from './transactions/getTransactions.js';
+import { checkToken } from '../middlewares/checkToken.js';
 
 const router = Router();
 
 router.use('/auth', authRegister);
 router.use('/auth', authLogin);
 router.use('/auth', authLogout);
+
+router.use('/transactions', checkToken, getTransactions);
 
 export default router;
