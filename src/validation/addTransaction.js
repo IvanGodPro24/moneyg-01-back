0327bc4541f1ef632555418076cf4ab6ac46d6b3
Dmitry Naumenko <dimanaumenko31@gmail.com>
2025-04-27 17:32:28 +0300

    delete required

--- src/validation/addTransaction.js
+++ src/validation/addTransaction.js
@@ -2,20 +2,18 @@ import Joi from 'joi';
 
 export const createTransactionSchema = Joi.object({
   type: Joi.string().valid('income', 'expense').required(),
-  category: Joi.string()
-    .valid(
-      'Main expenses',
-      'Products',
-      'Car',
-      'Self care',
-      'Child care',
-      'Household products',
-      'Education',
-      'Leisure',
-      'Other expenses',
-      'Entertainment',
-    )
-    .required(),
+  category: Joi.string().valid(
+    'Main expenses',
+    'Products',
+    'Car',
+    'Self care',
+    'Child care',
+    'Household products',
+    'Education',
+    'Leisure',
+    'Other expenses',
+    'Entertainment',
+  ),
   date: Joi.string().required(),
   sum: Joi.number().required(),
   comment: Joi.string().required(),
